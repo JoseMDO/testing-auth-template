1913c968e64c53b4b46e087c7e427a6938e72367
JoseMDO <joseamontesdeocamo@lewisu.edu>
2023-12-03 15:21:20 -0600

    updated id's

--- passport.js
+++ passport.js
@@ -3,11 +3,11 @@ const GitHubStrategy = require('passport-github2')
 const passport = require('passport')
 
 //https://console.cloud.google.com/apis/credentials
-const GOOGLE_CLIENT_ID = "20848583384-k3kceqk9idii833arekke8gvaqonegdf.apps.googleusercontent.com"
-const GOOGLE_CLIENT_SECRET = "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"
+const GOOGLE_CLIENT_ID = "72103332887-upik5p08qtg2cggbb8u1auj9s0d470tt.apps.googleusercontent.com"
+const GOOGLE_CLIENT_SECRET = "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"
 
-const GITHUB_CLIENT_ID = "03d4ebcb02c54cf4ed6e"
-const GITHUB_CLIENT_SECRET = "b9ffed28690453b4c2826e88ad4cc5931003de18"
+const GITHUB_CLIENT_ID = "2fb171dce9bfc15384f4"
+const GITHUB_CLIENT_SECRET = "ea2d2f70cbe1ce4714d38758648a449b2f1a169c"
 
 
 passport.use(new GoogleStrategy({
